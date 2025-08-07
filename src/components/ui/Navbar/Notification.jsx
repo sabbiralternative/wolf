56b07fcde52c9b9878ac4ad21a115ed26f03f882
sabbirHossain <sabbirshnt@gmail.com>
2025-08-07 14:29:19 +0600

    done

--- src/components/ui/Navbar/Notification.jsx
+++ src/components/ui/Navbar/Notification.jsx
@@ -17,9 +17,8 @@ const Notification = () => {
       notification?.length > 0
     ) {
       setShowNotification(true);
-      const sortNotification = notification?.sort((a, b) => a.sort - b.sort);
 
-      setFilteredNotification(sortNotification);
+      setFilteredNotification(notification);
     }
     if (
       notification?.length > 0 &&
@@ -32,10 +31,7 @@ const Notification = () => {
       );
 
       if (filteredNotifications?.length > 0) {
-        const sortNotification = filteredNotifications?.sort(
-          (a, b) => a.sort - b.sort
-        );
-        setFilteredNotification(sortNotification);
+        setFilteredNotification(filteredNotifications);
         setShowNotification(true);
       }
     }
